9d1898285f9ec0ba0421ddeb7bc7da7861d63804
Mohamed <mlangi@architectnow.net>
2017-10-14 07:47:38 -0500

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,7 @@ const routes: Routes = [
       component: DashboardComponent,
       children:[
         {
-          path:'parks',
+          path:'parks', 
           component:ParksComponent
         },
         {

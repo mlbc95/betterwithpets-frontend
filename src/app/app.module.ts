5c153358d187636adfdd43f4edc539be586768ac
Mohamed <mlangi@architectnow.net>
2017-10-14 01:47:27 -0500

    updated links to home page, style need to be change...UGLY

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { HomeComponent } from './component/home/home.component';
 import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './component/login/login.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
-import { NavbarComponent } from './component/src/app/component/navbar/navbar.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent }
 ];

c57094922a4ed33229d36aa9d8f8b70b76cfa014
Mohamed <mlangi@architectnow.net>
2017-10-14 08:35:50 -0500

    redierected after login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,8 @@ export class LoginComponent implements OnInit {
   model: any;
 
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    public router:Router
   ) {
     this.model = {};
   }
@@ -31,7 +33,7 @@ export class LoginComponent implements OnInit {
       (data: any): void => {
         if(data.success) {
           console.log(data.success);
-          //this.authService.storeAdminData(data.token, data.admin);
+          this.authService.storeUserData(data.token, data.admin);
           // this.flashMessagesService.show(
           //   'You are logged in',
           //   {
@@ -39,10 +41,10 @@ export class LoginComponent implements OnInit {
           //     timeout: 3000
           //   }
           // );
-          // this.router.navigate(['/register']);
+          this.router.navigate(['/dashboard']);
         } else {
           console.log(data);
-          //this.router.navigate(['/register']);
+          this.router.navigate(['/register']);
         }
       },
       (err: Error): void => {
